8e3687734b226191f7b2f03a7a33768d35ef5378
jihye0623 <wisdxx@gmail.com>
2024-03-26 19:51:35 +0900

    Setting

---
+++ src/main/java/com/aeon/hadog/HadogApplication.java
@@ -0,0 +1,13 @@
+package com.aeon.hadog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HadogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HadogApplication.class, args);
+	}
+
+}
