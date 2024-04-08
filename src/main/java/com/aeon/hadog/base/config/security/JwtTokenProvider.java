cbf6b65a494257fe876f6ba84c8c9248b616216e
Jihye0623 <wisdxx@gmail.com>
2024-04-08 15:47:01 +0900

    Feat: Jwt Security 기능 추가

---
+++ src/main/java/com/aeon/hadog/base/config/security/JwtTokenProvider.java
@@ -0,0 +1,48 @@
+package com.aeon.hadog.base.config.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
+public class JwtTokenProvider {
+
+    private static final String SECRET_KEY = "NMA8JPctFuna59f5";
+
+    // JWT Token 발급
+    public static String createToken(String loginId) {
+        // Claim = Jwt Token에 들어갈 정보
+        // Claim에 loginId를 넣어 줌으로써 나중에 loginId를 꺼낼 수 있음
+        Claims claims = Jwts.claims();
+        claims.put("loginId", loginId);
+
+        Date expiryDate=Date.from(Instant.now().plus(1, ChronoUnit.DAYS));
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(expiryDate)
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+
+    // Claims에서 loginId 꺼내기
+    public static String getLoginId(String token) {
+        return extractClaims(token).get("loginId").toString();
+    }
+
+    // 밝급된 Token이 만료 시간이 지났는지 체크
+    public static boolean isExpired(String token) {
+        Date expiredDate = extractClaims(token).getExpiration();
+        // Token의 만료 날짜가 지금보다 이전인지 check
+        return expiredDate.before(new Date());
+    }
+
+    // SecretKey를 사용해 Token Parsing
+    private static Claims extractClaims(String token) {
+        return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody();
+    }
+}
