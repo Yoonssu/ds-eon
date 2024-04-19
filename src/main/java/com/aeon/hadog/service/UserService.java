7dd8841a8f50cc7d4804fc43e2b74f7e26f2c83b
Jihye0623 <wisdxx@gmail.com>
2024-04-19 14:50:33 +0900

    Refactor: 비밀번호 유효성 검사
    - 이름, 아이디, 비밀번호, 닉네임, 이메일 필수 입력
    - 아이디는 5~16자 영문 대소문자와 숫자만 가능
    - 비밀번호는 8~16자 영문 대소문자와 숫자, 특수문자 가능
    - 닉네임은 특수문제를 제외한 2~10자리 문자

--- src/main/java/com/aeon/hadog/service/UserService.java
+++ src/main/java/com/aeon/hadog/service/UserService.java
@@ -11,7 +11,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.Errors;
+import org.springframework.validation.FieldError;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 @Service
@@ -93,4 +97,17 @@ public class UserService {
 
         return true;
     }
+
+    public Map<String, String> validateHandling(Errors errors) {
+        Map<String, String> validatorResult = new HashMap<>();
+
+        /* 유효성 검사에 실패한 필드 목록을 받음 */
+        for (FieldError error : errors.getFieldErrors()) {
+            String validKeyName = String.format("valid_%s", error.getField());
+            validatorResult.put(validKeyName, error.getDefaultMessage());
+        }
+
+        return validatorResult;
+    }
+
 }
