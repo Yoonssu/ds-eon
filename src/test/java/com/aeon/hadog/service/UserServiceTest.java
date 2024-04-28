1a87ac43b2059597d5d8f70c450b219d9537681d
Jihye0623 <wisdxx@gmail.com>
2024-04-28 23:17:59 +0900

    Test: UserServiceTest 추가
    회원 가입, 로그인 테스트 구현
    아이디, 닉네임, 이메일 테스트 구현

--- src/test/java/com/aeon/hadog/service/UserServiceTest.java
+++ src/test/java/com/aeon/hadog/service/UserServiceTest.java
@@ -1,46 +1,33 @@
 package com.aeon.hadog.service;
 
 import com.aeon.hadog.base.dto.user.JoinRequestDTO;
+import com.aeon.hadog.base.dto.user.LoginRequestDTO;
 import com.aeon.hadog.domain.User;
 import com.aeon.hadog.repository.UserRepository;
-import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.test.web.reactive.server.JsonPathAssertions;
-import org.springframework.test.web.servlet.MockMvc;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-import static org.mockito.Mockito.*;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
-@AutoConfigureMockMvc
 class UserServiceTest {
+
+    @Autowired
+    private UserService userService;
 
     @Mock
     private UserRepository userRepository;
 
-    @Mock
-    private PasswordEncoder passwordEncoder;
-
-    @InjectMocks
-    private UserService userService;
-
-    @Autowired
-    private MockMvc mockMvc;
-
-
     @Test
+    @DisplayName("회원가입 테스트")
     void signup() {
         // given
-        JoinRequestDTO joinRequestDTO = JoinRequestDTO.builder()
+        JoinRequestDTO dto = JoinRequestDTO.builder()
                 .name("김민지")
                 .id("minji01")
                 .password("minji01@")
@@ -48,57 +35,76 @@ class UserServiceTest {
                 .email("minji01@gmail.com")
                 .build();
 
-        when(userRepository.existsById(joinRequestDTO.getId())).thenReturn(false);
-        when(userRepository.existsByNickname(joinRequestDTO.getNickname())).thenReturn(false);
-        when(userRepository.existsByEmail(joinRequestDTO.getEmail())).thenReturn(false);
-
         // when
-        Long userId = userService.signup(joinRequestDTO);
+        Long userId = userService.signup(dto);
 
         // then
-        User findUser = userRepository.findByUserId(userId).orElseThrow();
-        assertThat(findUser.getUserId(), equalTo(userId));
-        verify(userRepository, times(1)).save(any(User.class));
+        assertNotNull(userId);
+        User findUser= userRepository.findByUserId(userId).orElseThrow();
+        assertEquals(findUser.getUserId(), userId);
     }
 
 
     @Test
+    @DisplayName("로그인 테스트")
     void signin() {
         // given
+        String id = "minji01";
+        String password = "minji01@";
+        LoginRequestDTO dto = LoginRequestDTO.builder()
+                .id(id)
+                .password(password)
+                .build();
 
         // when
+        String token = userService.signin(dto);
 
         // then
+        assertNotNull(token);
+        assertEquals(182, token.length());
     }
 
     @Test
+    @DisplayName("아이디 중복 테스트")
     void checkId() {
         // given
+        String existingId = "minji01";
 
         // when
+        boolean result = userService.checkId(existingId);
 
         // then
+        assertTrue(result);
     }
 
     @Test
+    @DisplayName("닉네임 중복 테스트")
     void checkNickName() {
         // given
+        String existingNickname = "김민지01";
 
         // when
+        boolean result = userService.checkNickName(existingNickname);
 
         // then
+        assertTrue(result);
     }
 
     @Test
+    @DisplayName("이메일 중복 테스트")
     void checkEmail() {
         // given
+        String existingEmail = "minji01@gmail.com";
 
         // when
+        boolean result = userService.checkEmail(existingEmail);
 
         // then
+        assertTrue(result);
     }
 
     @Test
+    @DisplayName("비밀번호 변경 테스트")
     void modifyPassword() {
         // given
 
@@ -108,6 +114,7 @@ class UserServiceTest {
     }
 
     @Test
+    @DisplayName("회원 탈퇴 테스트")
     void deleteUser() {
         // given
 
