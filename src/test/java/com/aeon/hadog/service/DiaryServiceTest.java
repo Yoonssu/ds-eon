0dfdfe6a34c2d4a4b97cd46ae556741526b37c1c
Jihye0623 <wisdxx@gmail.com>
2024-05-27 18:33:36 +0900

    Test: @nested 어노테션으로 테스트 코드 가독성 있게 작성

--- src/test/java/com/aeon/hadog/service/DiaryServiceTest.java
+++ src/test/java/com/aeon/hadog/service/DiaryServiceTest.java
@@ -9,6 +9,7 @@ import com.aeon.hadog.domain.User;
 import com.aeon.hadog.repository.DiaryRepository;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,131 +31,148 @@ class DiaryServiceTest {
     @Autowired
     DiaryRepository diaryRepository;
 
-    @Test
+    @Nested
     @DisplayName("일기 생성 테스트")
-    void createDiary() {
-        // given
-        String userId = "user3";
-        DiaryDTO dto = DiaryDTO.builder()
-                .emotionTrackId(1L)
-                .content("해피가 행복해보인다")
-                .build();
+    class createDiaryTest {
 
-        // when
-        Long diaryId = diaryService.createDiary(userId, dto);
+        @Test
+        @DisplayName("일기 생성 성공")
+        void createDiary() {
+            // given
+            String userId = "user3";
+            DiaryDTO dto = DiaryDTO.builder()
+                    .emotionTrackId(1L)
+                    .content("해피가 행복해보인다")
+                    .build();
 
-        // then
-        assertNotNull(diaryId);
-        Diary diary = diaryRepository.findByDiaryId(diaryId).orElseThrow();
-        assertEquals(diary.getDiaryId(), diaryId);
-    }
+            // when
+            Long diaryId = diaryService.createDiary(userId, dto);
 
-    @Test
-    @DisplayName("일기 생성 실패 테스트 - userError")
-    void createDiary_userError() {
-        // given
-        String userId = "user2";
-        DiaryDTO dto = DiaryDTO.builder()
-                .emotionTrackId(1L)
-                .content("해피가 행복해보인다")
-                .build();
+            // then
+            assertNotNull(diaryId);
+            Diary diary = diaryRepository.findByDiaryId(diaryId).orElseThrow();
+            assertEquals(diary.getDiaryId(), diaryId);
+        }
 
-        // when
-        Exception exception = assertThrows(EmotionTrackNotBelongToUserException.class, () -> {
-            diaryService.createDiary(userId, dto);
-        });
+        @Test
+        @DisplayName("일기 생성 실패")
+        void createDiary_userError() {
+            // given
+            String userId = "user2";
+            DiaryDTO dto = DiaryDTO.builder()
+                    .emotionTrackId(1L)
+                    .content("해피가 행복해보인다")
+                    .build();
 
-        // then
-        assertEquals(ErrorCode.EMOTION_TRACK_NOT_BELONG_TO_USER_ERROR, ((EmotionTrackNotBelongToUserException) exception).getErrorCode());
-    }
+            // when
+            Exception exception = assertThrows(EmotionTrackNotBelongToUserException.class, () -> {
+                diaryService.createDiary(userId, dto);
+            });
 
-    @Test
-    @DisplayName("일기 상세 테스트")
-    void getDiary() {
-        // given
-        String userId = "user3";
-        Long diaryId = 3L;
-        Diary diary = diaryRepository.findByDiaryId(3L).orElseThrow();
-
-        // when
-        DiaryDTO dto = diaryService.getDiary(userId, diaryId);
-
-        // then
-        assertNotNull(dto);
-        assertEquals(diary.getEmotionTrack().getEmotionTrackId(), dto.getEmotionTrackId());
-        assertEquals(diary.getDiaryDate(), dto.getDiaryDate());
-        assertEquals(diary.getContent(), dto.getContent());
-    }
-
-    @Test
-    @DisplayName("일기 상세 실패 테스트 - userError")
-    void getDiary_userError() {
-        // given
-        String userId = "user2";
-        Long diaryId = 3L;
-
-        // when
-        Exception exception = assertThrows(EmotionTrackNotBelongToUserException.class, () -> {
-            diaryService.getDiary(userId, diaryId);
-        });
-
-        // then
-        assertEquals(ErrorCode.EMOTION_TRACK_NOT_BELONG_TO_USER_ERROR, ((EmotionTrackNotBelongToUserException) exception).getErrorCode());
+            // then
+            assertEquals(ErrorCode.EMOTION_TRACK_NOT_BELONG_TO_USER_ERROR, ((EmotionTrackNotBelongToUserException) exception).getErrorCode());
+        }
 
     }
 
-    @Test
-    @DisplayName("일기 수정 테스트")
-    void modifyDiary() {
-        // given
-        String userId = "user3";
-        Long diaryId = 3L;
-        String content = "해피가 슬퍼보인다.";
 
-        // when
-        DiaryDTO dto = diaryService.modifyDiary(userId, diaryId, content);
-        Diary diary = diaryRepository.findByDiaryId(3L).orElseThrow();
+    @Nested
+    @DisplayName("일기 상세 불러오기 테스트")
+    class getDiaryTest {
+        @Test
+        @DisplayName("상세 불러오기 성공")
+        void getDiary() {
+            // given
+            String userId = "user3";
+            Long diaryId = 3L;
+            Diary diary = diaryRepository.findByDiaryId(3L).orElseThrow();
 
-        // then
-        assertNotNull(dto);
-        assertEquals(diary.getEmotionTrack().getEmotionTrackId(), dto.getEmotionTrackId());
-        assertEquals(diary.getDiaryDate(), dto.getDiaryDate());
-        assertEquals(diary.getContent(), dto.getContent());
+            // when
+            DiaryDTO dto = diaryService.getDiary(userId, diaryId);
+
+            // then
+            assertNotNull(dto);
+            assertEquals(diary.getEmotionTrack().getEmotionTrackId(), dto.getEmotionTrackId());
+            assertEquals(diary.getDiaryDate(), dto.getDiaryDate());
+            assertEquals(diary.getContent(), dto.getContent());
+        }
+
+        @Test
+        @DisplayName("상세 불러오기 실패")
+        void getDiary_userError() {
+            // given
+            String userId = "user2";
+            Long diaryId = 3L;
+
+            // when
+            Exception exception = assertThrows(EmotionTrackNotBelongToUserException.class, () -> {
+                diaryService.getDiary(userId, diaryId);
+            });
+
+            // then
+            assertEquals(ErrorCode.EMOTION_TRACK_NOT_BELONG_TO_USER_ERROR, ((EmotionTrackNotBelongToUserException) exception).getErrorCode());
+
+        }
+
     }
 
-    @Test
-    @DisplayName("일기 수정 실패 테스트 - userError")
-    void modifyDiary_userError() {
-        // given
-        String userId = "user2";
-        Long diaryId = 3L;
-        String content = "해피가 슬퍼보인다.";
+    @Nested
+    @DisplayName("일기 수정하기 테스트")
+    class modifyDiaryTest {
+        @Test
+        @DisplayName("일기 수정 테스트")
+        void modifyDiary() {
+            // given
+            String userId = "user3";
+            Long diaryId = 3L;
+            String content = "해피가 슬퍼보인다.";
 
-        // when
-        Exception exception = assertThrows(EmotionTrackNotBelongToUserException.class, () -> {
-            diaryService.modifyDiary(userId, diaryId, content);
-        });
+            // when
+            DiaryDTO dto = diaryService.modifyDiary(userId, diaryId, content);
+            Diary diary = diaryRepository.findByDiaryId(3L).orElseThrow();
 
-        // then
-        assertEquals(ErrorCode.EMOTION_TRACK_NOT_BELONG_TO_USER_ERROR, ((EmotionTrackNotBelongToUserException) exception).getErrorCode());
+            // then
+            assertNotNull(dto);
+            assertEquals(diary.getEmotionTrack().getEmotionTrackId(), dto.getEmotionTrackId());
+            assertEquals(diary.getDiaryDate(), dto.getDiaryDate());
+            assertEquals(diary.getContent(), dto.getContent());
+        }
 
-    }
+        @Test
+        @DisplayName("일기 수정 실패 테스트 - userError")
+        void modifyDiary_userError() {
+            // given
+            String userId = "user2";
+            Long diaryId = 3L;
+            String content = "해피가 슬퍼보인다.";
 
-    @Test
-    @DisplayName("일기 수정 실패 테스트 - valueError")
-    void modifyDiary_valueError() {
-        // given
-        String userId = "user3";
-        Long diaryId = 3L;
-        String content = "";
+            // when
+            Exception exception = assertThrows(EmotionTrackNotBelongToUserException.class, () -> {
+                diaryService.modifyDiary(userId, diaryId, content);
+            });
 
-        // when
-        Exception exception = assertThrows(BlanckContentException.class, () -> {
-            diaryService.modifyDiary(userId, diaryId, content);
-        });
+            // then
+            assertEquals(ErrorCode.EMOTION_TRACK_NOT_BELONG_TO_USER_ERROR, ((EmotionTrackNotBelongToUserException) exception).getErrorCode());
 
-        // then
-        assertEquals(ErrorCode.BLANK_CONTENT_ERROR, ((BlanckContentException) exception).getErrorCode());
+        }
+
+        @Test
+        @DisplayName("일기 수정 실패 테스트 - valueError")
+        void modifyDiary_valueError() {
+            // given
+            String userId = "user3";
+            Long diaryId = 3L;
+            String content = "";
+
+            // when
+            Exception exception = assertThrows(BlanckContentException.class, () -> {
+                diaryService.modifyDiary(userId, diaryId, content);
+            });
+
+            // then
+            assertEquals(ErrorCode.BLANK_CONTENT_ERROR, ((BlanckContentException) exception).getErrorCode());
+
+        }
 
     }
 
